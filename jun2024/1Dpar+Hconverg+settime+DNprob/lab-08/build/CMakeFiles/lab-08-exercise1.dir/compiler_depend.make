ca04caaf8341ac8c3d2c67a2797d2837d13afc4b
Davide Villani <davidevillani2002@gmail.com>
2025-05-07 16:23:33 +0200

    removed files

--- jun2024/1Dpar+Hconverg+settime+DNprob/lab-08/build/CMakeFiles/lab-08-exercise1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for lab-08-exercise1.
-# This may be replaced when dependencies are built.
--- jun2024/1Dpar+Hconverg+settime+DNprob/lab-08/build/cmake_install.cmake
+++ jun2024/1Dpar+Hconverg+settime+DNprob/lab-08/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/davide-villani/Desktop/PDE/tde06/1Dpar+Hconverg+settime+DNprob/lab-08
+# Install script for directory: /home/davide-villani/Desktop/UNI/PDE/solutions/NM4PDE/jun2024/1Dpar+Hconverg+settime+DNprob/lab-08
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/u/sw/toolchains/gcc-glibc/11.2.0/prefix/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/davide-villani/Desktop/PDE/tde06/1Dpar+Hconverg+settime+DNprob/lab-08/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/davide-villani/Desktop/UNI/PDE/solutions/NM4PDE/jun2024/1Dpar+Hconverg+settime+DNprob/lab-08/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
